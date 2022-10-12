2f6c95138b700d504a6bea719f66cf126c2647d2
n.rasullu <n.rasullu@STHQ0124-01>
2022-10-12 21:05:56 +0400

    Add project files.

---
+++ Project5/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+
+//class Engine {
+//public:
+//	Engine() { cout << "default\n"; }
+//	Engine(bool b) { cout << "with params\n"; }
+//	Engine(const Engine& o) { cout << "copy\n"; }
+//	~Engine() { cout << "destructor\n"; }
+//};
+//
+//Engine foo(Engine e) {
+//	return 1;
+//}
+//
+//void main() {
+//	Engine o;
+//	foo(o);
+//
+//	// Ayshan - - - - 1
+//	// Bahruz + - - - .
+//	// Elchin - - - - 2
+//	// Nigar  + - - - 3
+//	// Rasul  - - - - 1 
+//	// Sarxan - - - - 1
+//	// Vamil  - + + + 9
+//	// Huseyn - - - - 1
+//	// Islam  + + + + 12
+//	// Azar   + + + + 12
+//	// Ruso   - + - + 6
+//	// Online + - + + 9
+//}
